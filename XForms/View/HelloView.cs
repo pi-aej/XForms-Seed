dc020c1bd1ea52f8c31980bc7096c0fed11c6a8b
Alex Jarkey <alex.jarkey@planetinnovation.com.au>
2015-05-09 09:35:16 +1000

    Moved presentation into HelloView type

---
+++ XForms/View/HelloView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XForms
+{
+	public class HelloView : ContentPage
+	{
+		public HelloView ()
+		{
+			Content = new Label {
+				Text = "Hello, Forms!",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+		}
+	}
+}
+
